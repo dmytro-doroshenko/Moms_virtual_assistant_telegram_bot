f4c1ad30119658532e71a9f4aaa8f1de0e2973b2
Iryna Hotsiy <iryna.hotsiy@gmail.com>
2020-08-01 09:15:54 +0300

    fix dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN || 'token not configured',
     MONGO_DB_URL: process.env.MONGO_DB_URL || 'http://localhost:27017',
