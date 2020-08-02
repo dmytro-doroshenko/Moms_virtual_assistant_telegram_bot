b98cd614618c838bc01fb5f0ab9545026f1c836b
ciberpop <jelrainverner@gmail.com>
2020-08-02 16:26:40 +0300

    add winston-cloudwatch configuration

--- lambda.js
+++ lambda.js
@@ -1,16 +1,31 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
-const bot = require("./src/bot");
-const {appConfigs: {MONGO_DB_URL}} = require('./src/config');
+const app = express();
+
+const bot = require('./src/bot');
+const { appConfigs: { MONGO_DB_URL }, logger } = require('./src/config');
 
 exports.handler = async (event) => {
-    await mongoose.connect(MONGO_DB_URL, {
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      useFindAndModify: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+  await mongoose.connect(MONGO_DB_URL, {
+    bufferCommands: false,
+    bufferMaxEntries: 0,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  app.use('*', (err, req, res, next) => {
+    logger.error({
+      method: req.method,
+      url: req.path,
+      data: req.body,
+      time: new Date(),
+      message: err.message,
     });
+
+    next(err);
+  });
 
   const update = JSON.parse(event.body); // get data passed to us
   await bot.handleUpdate(update); // make Telegraf process that data
