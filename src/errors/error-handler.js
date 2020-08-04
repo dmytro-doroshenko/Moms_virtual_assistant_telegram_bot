e0ea20b1fd28e49fc5a145417155880dd432eab1
ciberpop <jelrainverner@gmail.com>
2020-08-05 00:49:33 +0300

    Fix file error-handler.js

--- src/errors/error-handler.js
+++ src/errors/error-handler.js
@@ -2,5 +2,4 @@ const {logger} = require('../config');
 
 module.exports = (err, ctx) => {
     logger.error(`Error: ${err.message}, User: ${ctx.from.username}`);
-    console.log(`Error: ${err.message}, User: ${ctx.from.username}`);
 };
