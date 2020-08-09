06045275402d40e7970a4e33215af18b8bfadec6
ciberpop <jelrainverner@gmail.com>
2020-08-09 21:24:22 +0300

    Add chatbase configuration

--- src/config/config.js
+++ src/config/config.js
@@ -12,4 +12,9 @@ module.exports = {
   CLOUDWATCH_ACCESS_KEY: process.env.CLOUDWATCH_ACCESS_KEY,
   CLOUDWATCH_SECRET_ACCESS_KEY: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
   CLOUDWATCH_REGION: process.env.CLOUDWATCH_REGION || 'us-east-1',
+
+  CHATBASE_API_KEY: process.env.CHATBASE_API_KEY || 'wrong chatbase api key',
+  CHATBASE_PLATFORM: process.env.CHATBASE_PLATFORM || 'GoogleAssistant',
+  CHATBASE_USER_ID: process.env.CHATBASE_USER_ID || 'bkkjnnknknk',
+  CHATBASE_VERSION: process.env.CHATBASE_VERSION || '2.0',
 };
--- src/services/dialogflow.js
+++ src/services/dialogflow.js
@@ -1,15 +1,31 @@
+const chatbase = require('@google/chatbase');
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
 
-const {appConfigs: {DIALOGFLOW_PROJECT_ID}, logger} = require('../config');
+const {
+  appConfigs: {
+    DIALOGFLOW_PROJECT_ID,
+    CHATBASE_API_KEY,
+    CHATBASE_PLATFORM,
+    CHATBASE_USER_ID,
+    CHATBASE_VERSION,
+  },
+  logger,
+} = require('../config');
+
+chatbase
+  .setApiKey(CHATBASE_API_KEY)
+  .setPlatform(CHATBASE_PLATFORM)
+  .setUserId(CHATBASE_USER_ID)
+  .setVersion(CHATBASE_VERSION);
 
 module.exports = {
   detectIntent: async (message, languageCode) => {
     const sessionId = uuid.v4();
     // Create a new session
     const sessionClient = new dialogflow.SessionsClient({
-      keyFilename: __dirname + '../../../google-cloud-key.json'
-      });
+      keyFilename: `${__dirname}../../../google-cloud-key.json`,
+    });
     const sessionPath = sessionClient.sessionPath(DIALOGFLOW_PROJECT_ID, sessionId);
 
     // The text query request.
@@ -24,19 +40,49 @@ module.exports = {
         },
       },
     };
- 
-    // Send request and log result
+
+    sessionClient
+        .detectIntent(request)
+        .then((responses) => {
+          logger.info('Detected intent');
+          const { queryResult } = responses[0];
+          const { queryText, fulfillmentText, intent } = queryResult;
+          logger.info(`  Query: ${queryText}`);
+          logger.info(`  Response: ${fulfillmentText}`);
+
+          if (intent) {
+            logger.info(`  Intent: ${intent.displayName}`);
+            return chatbase
+              .newMessage()
+              .setIntent(intent.displayName)
+              .setMessage(fulfillmentText)
+              .send();
+          }
+          logger.info('No intent matched.');
+          return chatbase
+            .newMessage()
+            .setAsNotHandled()
+            .setMessage(fulfillmentText)
+            .send();
+        })
+        .then(() => {
+          console.log('send fulfillmentText');
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+    //   Send request and log result
     const responses = await sessionClient.detectIntent(request);
     const result = responses[0].queryResult;
-    logger.info(`Query: ${result.queryText}`);
+    //    logger.info(`Query: ${result.queryText}`);
     const answer = result.fulfillmentText;
-    logger.info(`Response: ${answer}`);
+    //    logger.info(`Response: ${answer}`);
     if (result.intent) {
-      logger.info(`Intent matched: ${result.intent.displayName}`);
+      //      logger.info(`Intent matched: ${result.intent.displayName}`);
       return answer;
-    } else {
-      logger.info('No intent matched.');
-      return null;
     }
-  }
-}
+    //    logger.info('No intent matched.');
+    return null;
+  },
+};
