cc0279e10d29b6b37023d93f09968a9201bb1dd2
Iryna Hotsiy <iryna.hotsiy@gmail.com>
2020-08-15 23:32:31 +0300

    fix nlp answer middelware

--- src/middlewares/get-nlp-answer.middleware.js
+++ src/middlewares/get-nlp-answer.middleware.js
@@ -8,17 +8,18 @@ module.exports = async (ctx, next) => {
     const userMessage = ctx.update.message.text;
     const result = await detectIntent(userMessage, chosenLanguage);
 
-    if (result.intentDetectionConfidence < 0.95) {
+    if (result === null || result.intentDetectionConfidence < 0.5) {
+        return next();
+    } 
+
+    if (result.intentDetectionConfidence < 0.95 && result.intentDetectionConfidence > 0.5){
 
         const responseIntent = await getIntentTrainingPhrase(result.intentName, chosenLanguage);
         const message = `${GET_TRAINING_PHRASE_PART_ONE[chosenLanguage]} \n ❓${responseIntent} \n 👉${result.answer} \n\n${GET_TRAINING_PHRASE_PART_TWO[chosenLanguage]} "${FAQ[chosenLanguage]}"`;
 
         return ctx.reply(message);
-    }
-    else {
-        
-        return ctx.reply(result.answer);
-    }
+    } 
 
-    return next();
+    return ctx.reply(result.answer);
+   
 };
